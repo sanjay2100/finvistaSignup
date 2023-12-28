56bd451871308ec7b69045a032eb8b5761837e6e
sanjay <sanjaymanikandan21@gmail.com>
2023-12-28 09:47:29 +0530

    Folder change

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Stack, Box } from '@mui/material'
-import RightImg from '../components/Signup/rightside/RightImg'
-import SignupForm from '../components/Signup/FormComponents/SignupForm'
-import ButtonAppBar from '../components/Signup/Appbar/SignupAppbar'
-import Partner from '../components/Signup/Partner/Partner'
+import RightImg from '../../components/Signup/rightside/RightImg'
+import SignupForm from '../../components/Signup/FormComponents/SignupForm'
+import ButtonAppBar from '../../components/Signup/Appbar/SignupAppbar'
+import Partner from '../../components/Signup/Partner/Partner'
 
 
 
@@ -21,7 +21,13 @@ function Signup() {
                 <RightImg />
            
             </Stack>
-            <Box width={{md:'50%',sm:'100%',xs:'100%'}} height='100%' display='flex' flexDirection='column' justifyContent='center' paddingRight={{md:'50px',sm:'0px'}} >
+            <Box 
+            width={{md:'50%',sm:'100%',xs:'100%'}} 
+            height='100%' 
+            display='flex' 
+            flexDirection='column' 
+            justifyContent='center' 
+            paddingRight={{md:'50px',sm:'0px'}} >
                 <SignupForm />
             </Box>
         </Stack>
